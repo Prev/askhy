c4ac8041af9e01166f83f1c1fbef51ec4caba9ce
Prev <prevdev@gmail.com>
2017-12-08 11:38:33 +0900

    Improve typos, Change some API URL to RESTful \
    Change API URL from `POST /cheer` to `POST /ask/{id}/cheer`

--- app/main.py
+++ app/main.py
@@ -9,12 +9,11 @@ app = Flask(__name__)
 # Init tables in db
 init_tables()
 
-"""
-Index page
-Show list of `asks`, and cheer count of each ask
-"""
 @app.route('/')
 def index():
+	""" Index page
+	  Show list of `asks`, and cheer count of each ask
+	"""
 	with get_db().cursor() as cursor :
 		cursor.execute("SELECT *, (SELECT COUNT(*) FROM `cheer` WHERE ask_id = ask.id) AS cheer_cnt FROM `ask`")
 		result = cursor.fetchall()
@@ -24,12 +23,13 @@ def index():
 	)
 
 
-"""
-Show detail of one `ask`
-See all cheers in this ask
-"""
 @app.route('/ask/<int:ask_id>', methods=['GET'])
 def view_ask(ask_id):
+	""" Show detail of one `ask`
+	  See all cheers in this ask
+
+	:param ask_id: Primary key of `ask` table
+	"""
 	conn = get_db()
 
 	with conn.cursor() as cursor :
@@ -49,14 +49,12 @@ def view_ask(ask_id):
 	)
 
 
-"""
-Add new ask
-
-[request params]
-  - message
-"""
 @app.route('/ask', methods=['POST'])
 def add_ask():
+	""" Add new ask
+
+	:post-param message: Message of `ask`
+	"""
 	conn = get_db()
 	message = request.form.get('message')
 
@@ -70,17 +68,15 @@ def add_ask():
 	return redirect("/#a" + str(id))
 
 
-"""
-Add new cheer
 
-[request params]
-  - ask_id
-  - message
-"""
-@app.route('/cheer', methods=['POST'])
-def add_cheer():
+@app.route('/ask/<int:ask_id>/cheer', methods=['POST'])
+def add_cheer(ask_id):
+	""" Add new cheer to ask
+
+	:param ask_id: Primary key of `ask` table
+	:post-param message: Message of `cheer`
+	"""
 	conn = get_db()
-	ask_id = request.form.get('ask_id')
 	message = request.form.get('message')
 
 	with conn.cursor() as cursor :
@@ -89,26 +85,23 @@ def add_cheer():
 
 	conn.commit()
 
-	back = request.form.get('back')
-	if back :
-		return redirect(back)
-	else :
-		return redirect("/#c" + ask_id)
+	redirect_url = request.form.get('back', '/#c' + str(ask_id))
+	return redirect(redirect_url)
 
 
-"""
-Template filter: <hide_ip_address>
-Hide last sections of IP address
 
-ex) 65.3.12.4 -> 65.3.*.*
-"""
 @app.template_filter()
 def hide_ip_address(ip_address):
+	"""
+	Template filter: <hide_ip_address>
+	Hide last sections of IP address
+
+	ex) 65.3.12.4 -> 65.3.*.*
+	"""
 	if not ip_address : return ""
 	else :
 		ipa = ip_address.split(".")
 		return "%s.%s.*.*" % (ipa[0], ipa[1])
-
 
 
 
